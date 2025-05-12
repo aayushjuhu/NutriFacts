6818ccfa1f479bdbb631636e6849b5a3f686adbe
aayushjuhu <89008608+aayushjuhu@users.noreply.github.com>
2025-05-12 11:26:02 +0530

    Delete nf directory

--- nf/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NfConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nf'
--- nf/urls.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from nf import views
-urlpatterns = [
-    path('', views.index, name="home")
-]
